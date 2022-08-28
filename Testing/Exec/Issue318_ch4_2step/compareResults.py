3bf576acd3767c6149d91f1a077e490a50a55f9b
Malik <malik.hassanaly@gmail.com>
2022-08-27 18:09:01 -0600

    plot result against cantera

--- Testing/Exec/Issue318_ch4_2step/compareResults.py
+++ Testing/Exec/Issue318_ch4_2step/compareResults.py
@@ -6,11 +6,15 @@ import matplotlib.pyplot as plt
 from plotsUtil import *
 
 filePP = 'PPreaction.txt'
+fileCT = 'CanteraReaction_2step.txt'
 
 A1 = io.readMultiColFile(filePP)
+A2 = io.readMultiColFile(fileCT)
 
 fig=plt.figure()
 plt.plot(A1[:,0],A1[:,1],linewidth=3,color='k',label='PelePhysics')
+plt.plot(A2[:,0],A2[:,1],'x',linewidth=3, color='b',label='Cantera')
 prettyLabels('time[s]','T[K]',14)
 plotLegend()
 plt.show()
+
