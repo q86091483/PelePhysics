a11d8ff9c88aea43c644c048f0bb3a5e28121e21
Malik <malik.hassanaly@gmail.com>
2022-08-27 18:06:42 -0600

    leanify exec script

--- Testing/Exec/Issue318_ch4_2step/exec.sh
+++ Testing/Exec/Issue318_ch4_2step/exec.sh
@@ -1,26 +1,17 @@
 #!/bin/bash
 
-# Find first executable name available
+# Compile
 make COMP=llvm TPL
 make COMP=llvm
 execname=`find . -name "Pele*.ex" | head -1`
 
-if [ -z "$execname" ]
-then
-    make COMP=llvm TPL
-    make COMP=llvm
-    execname=`find . -name "Pele*.ex" | head -1`
-fi
-
-
+# Execute HR test
 if [[ -f "PPreaction.txt" ]]; then
     rm PPreaction.txt
-fi
-if [[ -f "log" ]]; then
-    rm log
 fi
 $execname input
 
 
 
+# Plot results
 python compareResults.py
