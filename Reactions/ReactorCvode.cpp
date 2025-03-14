0f8c6f3ab9a611f3f88757ae146792694aee50f8
zisen <zisen347@gmail.com>
2025-03-14 16:50:28 -0400

    Remove Cvode for aux.

--- Reactions/ReactorCvode.cpp
+++ Reactions/ReactorCvode.cpp
@@ -1777,12 +1777,12 @@ ReactorCvode::react(
     absTol, m_typ_vals_aux, "cvode", verbose);
 
   // Actual CVODE solve
-  BL_PROFILE_VAR("Pele::ReactorCvode::react():CVode", AroundCVODE);
-  flag =
-    CVode(cvode_mem_aux, time_final, y_aux, &CvodeActual_time_final, CV_NORMAL);
-  if (utils::check_flag(&flag, "CVode", 1)) {
-    return (1);
-  }
+  //BL_PROFILE_VAR("Pele::ReactorCvode::react():CVode", AroundCVODE);
+  //flag =
+  //  CVode(cvode_mem_aux, time_final, y_aux, &CvodeActual_time_final, CV_NORMAL);
+  //if (utils::check_flag(&flag, "CVode", 1)) {
+  //  return (1);
+  //}
   BL_PROFILE_VAR_STOP(AroundCVODE);
 #endif
 
