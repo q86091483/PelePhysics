2b84f7596e334d767e5171c9fa8c01939cfdcada
zisen <zisen347@gmail.com>
2025-08-08 10:58:03 +0200

    Change threshold to 0E-4.

--- Source/Reactions/ReactorCvodeJacobian.cpp
+++ Source/Reactions/ReactorCvodeJacobian.cpp
@@ -381,7 +381,7 @@ cJac_aux(
       const int AGE_IN_J  = offset + AGE_IN_AUX  + i;
       amrex::Real* J_col_mixf = SM_COLUMN_D(J, MIXF_IN_J);
       amrex::Real* J_col_age  = SM_COLUMN_D(J, AGE_IN_J);
-      if ((ydata[MIXF_IN_J]/rho) > 1E-4) {
+      if ((ydata[MIXF_IN_J]/rho) > 0E-4) {
         J_col_mixf[AGE_IN_J] = 1 - (ydata[AGE_IN_J]/ydata[MIXF_IN_J]/ydata[MIXF_IN_J]) * rhoAuxsrc_ext[MIXF_IN_AUX+i];
         J_col_age[AGE_IN_J] = rhoAuxsrc_ext[MIXF_IN_AUX+i] / ydata[MIXF_IN_J];
       }
@@ -406,7 +406,7 @@ cJac_aux(
       amrex::Real* J_col_agepv  = SM_COLUMN_D(J, AGEPV_IN_J);
 
       //J_col_temp[AGEPV_IN_J]  = ydata[MIXF_IN_J] * dfdT;
-      if ((ydata[MIXF_IN_J]/rho) > 1E-4) {
+      if ((ydata[MIXF_IN_J]/rho) > 0E-4) {
         J_col_mixf[AGEPV_IN_J]  = f_T - (ydata[AGEPV_IN_J]/ydata[MIXF_IN_J]/ydata[MIXF_IN_J]) * rhoAuxsrc_ext[MIXF_IN_AUX+i];
         J_col_age[AGEPV_IN_J]   = 0.0;
         J_col_agepv[AGEPV_IN_J] = rhoAuxsrc_ext[MIXF_IN_AUX+i] / ydata[MIXF_IN_J];
